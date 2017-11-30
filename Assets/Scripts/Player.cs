04b01d9e5367c6b0a4e5a8855d1e6feaca7aa32a
Tristin Bridgeman <bridgemantristin@gmail.com>
2017-11-29 21:58:48 -0500

    player movement added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float m_health;
+	public float m_walkSpeed = 5f;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+
+	private Animator m_animator;
+	private CharacterController m_characterController;
+	private Rigidbody m_rb;
+	private AudioSource m_audio;
+
+	void Start() {
+		m_rb = GetComponent<Rigidbody>();
+		m_audio = GetComponent<AudioSource>();
+		m_characterController = GetComponent<CharacterController>();
+		m_animator = GetComponent<Animator>();
 	}
+
+
+	void Update() {
+		Vector3 m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		m_characterController.SimpleMove(m_moveDirection * m_walkSpeed);
+
+	}
+
+
+
 }
